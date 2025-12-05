e5c374ea16ea48c4d709587e98d3a97c1a5302aa
Suprim Khatri <Supremekhatri62@gmail.com>
2025-12-05 13:13:48 +0545

    added 0.0.0.0 for the app to listen

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -46,8 +46,8 @@ app.use(
   },
 )
 
-const PORT = process.env.PORT || 5000
+const PORT = Number(process.env.PORT) || 5000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`)
 })
